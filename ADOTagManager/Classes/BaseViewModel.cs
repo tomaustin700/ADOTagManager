698f2af0e37578ad3dbfec48d4b1eb40a8d47993
Tom Austin <Tom.Austin@streets-heaver.com>
2022-01-05 16:44:49 +0000

    Init

---
+++ ADOTagManager/Classes/BaseViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ADOTagManager.Classes
+{
+    public abstract class BaseViewModel : BaseNotify
+    {
+        List<DelegateCommandBase> _commands;
+
+        public BaseViewModel()
+        {
+            CommandManager.RequerySuggested += CommandManager_RequerySuggested;
+            Loaded();
+        }
+
+        public abstract void Loaded();
+
+        private void CommandManager_RequerySuggested(object sender, EventArgs e)
+        {
+            RaiseCanExecuteChangedOnCommands();
+        }
+
+        protected void RaiseCanExecuteChangedOnCommands()
+        {
+            if (_commands == null)
+            {
+                var delegateCommands = GetDelegateCommands(this.GetType(), this);
+                _commands = delegateCommands.Where(cm => cm != null).ToList();
+            }
+            if (_commands != null)
+                _commands.ForEach(cm => cm.RaiseCanExecuteChanged());
+
+            IEnumerable<DelegateCommandBase> GetDelegateCommands(Type type, object instance)
+            {
+                return type.GetProperties().Where(p => typeof(DelegateCommandBase).IsAssignableFrom(p.PropertyType)).Select(p => p.GetValue(instance) as DelegateCommandBase);
+            }
+        }
+    }
+}
---
+++ ADOTagManager/ViewModels/TagManagerViewModel.cs
@@ -0,0 +1,97 @@
+﻿using ADOTagManager.Classes;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.VisualStudio.Services.Client;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.Common.TokenStorage;
+using Microsoft.VisualStudio.Services.WebApi;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOTagManager.ViewModels
+{
+    public class TagManagerViewModel : BaseViewModel
+    {
+        #region Fields
+        private string _orgName;
+        private string _pat;
+        private WorkItemTrackingHttpClient _client;
+
+        #endregion
+
+        #region Properties
+
+
+        public string PAT
+        {
+            get { return _pat; }
+            set
+            {
+                _pat = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+        public string OrgName
+        {
+            get { return _orgName; }
+            set
+            {
+                _orgName = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+        #endregion
+
+        #region Constructor
+        public TagManagerViewModel()
+        {
+            LoginCommand = new DelegateCommand(Login, CanLogin);
+
+
+        }
+        #endregion
+
+        #region Commands
+
+        public DelegateCommand LoginCommand { get; set; }
+
+
+
+        #endregion
+
+        #region Methods
+
+        void Login()
+        {
+            VssBasicCredential cred = new VssBasicCredential(new NetworkCredential("", PAT));
+
+            var conn = new VssConnection(new Uri($"https://dev.azure.com/{OrgName}"), new VssCredentials(cred));
+            _client = conn.GetClient<WorkItemTrackingHttpClient>();
+
+        }
+
+        public override void Loaded()
+        {
+
+
+        }
+
+        public bool CanLogin()
+        {
+            return !string.IsNullOrEmpty(PAT) && !string.IsNullOrEmpty(OrgName);
+        }
+
+
+
+
+        #endregion
+    }
+}
